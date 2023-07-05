7cf2bb29aa87db4b9071e321cc869bbc63afd629
Hakeemshaik1 <130968068+Hakeemshaik1@users.noreply.github.com>
2023-07-05 12:30:41 +0200

    1SPT PUSH

---
+++ Assets/Scripts/BossBeatenText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BossBeatenText : MonoBehaviour
+{
+    public Text bossBeatenText;
+
+    private void Start()
+    {
+        bossBeatenText.enabled = false; // Hide the text initially
+        BossEventSystem.instance.onBossesBeaten.AddListener(OnBossesBeaten);
+    }
+
+    private void OnDestroy()
+    {
+        BossEventSystem.instance.onBossesBeaten.RemoveListener(OnBossesBeaten);
+    }
+
+    private void OnBossesBeaten()
+    {
+        bossBeatenText.enabled = true; // Show the text
+    }
+}
